7230d1cf7c5b0d935624506a3262a36fd71d5440
mzainabadin <30627311+mzainabadin@users.noreply.github.com>
2017-08-08 14:36:47 +0500

    Add files via upload

---
+++ singleton.php
@@ -0,0 +1,39 @@
+<?php
+
+class SingleClass
+{
+    static $object;
+    /**
+     *Function that checks if object has already been created of class SingleClass
+     * to make sure only single instance is created
+     *
+     *@param no parameters
+     *
+     *@return object of SingleClass
+     */
+    static public function getSingleton()
+    {
+        if(isset(self::$object) == False)
+        {
+            self::$object = new SingleClass();
+            echo "if statement";
+        }
+        return self::$object;
+    }
+     /**
+     *Constructor function just made it private to avoid calling it outside the class
+     *
+     *@param no parameters
+     *
+     *@return nothing
+     */
+    private function __construct()
+    {
+
+    }
+
+}
+$mySingle1 = SingleClass::getSingleton();   // one object made
+$mySingle2 = SingleClass::getSingleton();   // reference to first object returned
+$mySingle3 = SingleClass::getSingleton();   // reference to first object returned
+?>
