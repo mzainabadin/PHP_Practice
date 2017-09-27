607c7d098c099c7beb2c09327b18c0c4d98d2f88
mzainabadin <30627311+mzainabadin@users.noreply.github.com>
2017-09-27 19:21:12 +0500

    Add files via upload

---
+++ subpanels.php
@@ -0,0 +1,10 @@
+<?php
+//$viewdefs['module name']['base']['layout']['subpanels']['components'][]
+$viewdefs['key_status']['base']['layout']['subpanels']['components'][] = array (
+  'layout' => 'subpanel',
+  'label' => 'LBL_KEY_STUDENT_KEY_STATUS_FROM_KEY_STUDENT_TITLE',
+  'context' => 
+  array (
+    'link' => 'key_student_key_status',				//link field name created earlier
+  ),
+);
