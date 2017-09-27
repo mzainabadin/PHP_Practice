dcf47a3d8c3f0cc76dc03f9292307304740207e8
mzainabadin <30627311+mzainabadin@users.noreply.github.com>
2017-09-27 19:24:46 +0500

    Add files via upload

---
+++ app/subpanels.php
@@ -0,0 +1,40 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+
+
+$viewdefs['key_student']['base']['layout']['subpanels']['components'][] = array (
+  'layout' => 'subpanel',
+  'label' => 'LBL_KEY_STUDENT_KEY_STATUS_FROM_KEY_STATUS_TITLE',
+  'context' => 
+  array (
+    'link' => 'key_student_key_status',
+  ),
+);
+
+
+$viewdefs['key_student']['base']['layout']['subpanels']['components'][] = array (
+  'layout' => 'subpanel',
+  'label' => 'LBL_KEY_STUDENT_KEY_TEACHER_FROM_KEY_TEACHER_TITLE',
+  'context' => 
+  array (
+    'link' => 'key_student_key_teacher',
+  ),
+);
+
+$viewdefs['key_student']['base']['layout']['subpanels']['components'][] = array (
+  'layout' => 'subpanel',
+  'label' => 'LBL_KEY_STUDENT_KEY_TEACHER_FROM_KEY_TEACHER_TITLE',
+  'context' => 
+  array (
+    'link' => 'key_student_key_teacher',
+  ),
+);
+
+$viewdefs['key_student']['base']['layout']['subpanels']['components'][] = array (
+  'layout' => 'subpanel',
+  'label' => 'LBL_KEY_STUDENT_KEY_TEACHER_FROM_KEY_TEACHER_TITLE',
+  'context' => 
+  array (
+    'link' => 'key_student_key_teacher',
+  ),
+);
